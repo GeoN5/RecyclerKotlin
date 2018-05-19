bffe6d7904eb9a54b9bf30a8325c3b6a4e7f98b6
GeoN5 <sgepyh2916@naver.com>
2018-05-20 06:25:11 +0900

    Spelling correction

--- app/src/main/java/com/example/geonho/recyclerkotlin/MainActivity.kt
+++ app/src/main/java/com/example/geonho/recyclerkotlin/MainActivity.kt
@@ -9,8 +9,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var recyclerAdapter: RecyclerAdapter? = null
-    /*이거 2개를 null로 초기화해서 setRecyclerView함수에서 nullsafe로 호출해야하는데
-    * 이게 지금 null이라 실행이 안되는거같은데 그렇다고 초기화를 안하면 빨간 밑줄뜨는데...*/
     val dataList = arrayListOf<RecyclerModel>()  //기존 List는 immutable.
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +22,10 @@ class MainActivity : AppCompatActivity() {
         dataList.add(RecyclerModel(R.drawable.apple,"First : A","Apple"))
         dataList.add(RecyclerModel(R.drawable.banana,"First : B","Banana"))
         dataList.add(RecyclerModel(R.drawable.cherry,"First : C","Cherry"))
-        dataList.add(RecyclerModel(R.drawable.durian,"First : D","Dutian"))
+        dataList.add(RecyclerModel(R.drawable.durian,"First : D","Durian"))
         dataList.add(RecyclerModel(R.drawable.eggfruit,"First : E","Eggfruit"))
         dataList.add(RecyclerModel(R.drawable.fig,"First : F","Fig"))
-        dataList.add(RecyclerModel(R.drawable.grape,"First : G","Graple"))
+        dataList.add(RecyclerModel(R.drawable.grape,"First : G","Grape"))
     }
 
     fun setRecyclerView(){
